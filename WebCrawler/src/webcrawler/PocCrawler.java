3b18c69a7f9e8e250c998ad2126960a7cdcec804
rsdsilveira <rsdsilveira@gmail.com>
2014-08-30 20:27:02 -0300

    rsdsilveira

---
+++ WebCrawler/src/webcrawler/PocCrawler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package webcrawler;
+
+/**
+ *
+ * @author rsdsilveira
+ */
+public class PocCrawler {
+    
+    public void Run()
+    {
+        String site = "http://busca.americanas.com.br/busca.php?q=885909784912";
+   
+    }
+}
